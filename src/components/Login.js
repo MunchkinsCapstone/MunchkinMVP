da7bc4deec9af3c340a338efba2f91201b5480bf
bassgt95 <bassgt95@gmail.com>
2018-06-27 10:34:48 -0400

    fixed syntax errors

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,17 +3,17 @@ import React from 'react';
 const Login = (props) => {
 	return (
 		<form>
-			<div class='form-row'>
-				<div class='form-group col-md-6'>
-					<label for='inputEmail4'>Email</label>
-					<input type='email' class='form-control' id='inputEmail4' placeholder='Email' />
+			<div className='form-row'>
+				<div className='form-group col-md-6'>
+					<label htmlFor='inputEmail4'>Email</label>
+					<input type='email' className='form-control' id='inputEmail4' placeholder='Email' />
 				</div>
-				<div class='form-group col-md-6'>
-					<label for='inputPassword4'>Password</label>
-					<input type='password' class='form-control' id='inputPassword4' placeholder='Password' />
+				<div className='form-group col-md-6'>
+					<label htmlFor='inputPassword4'>Password</label>
+					<input type='password' className='form-control' id='inputPassword4' placeholder='Password' />
 				</div>
 			</div>
-			<button type='submit' class='btn btn-primary'>
+			<button type='submit' className='btn btn-primary'>
 				Sign in
 			</button>
 		</form>
