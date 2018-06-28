0ea79107790e07121ccb1b40d2579e619815cfb8
bassgt95 <bassgt95@gmail.com>
2018-06-28 13:40:31 -0400

    fixed equipment getter

--- src/Player.js
+++ src/Player.js
@@ -30,10 +30,9 @@ class Player {
       ),
       legs: new Equipment('Rags', 'default image', 'head', () => {}, () => {}),
       feet: new Equipment('Bare', 'default image', 'head', () => {}, () => {}),
-      // get bonus() {
-      //   return 0;
-      // }
-      bonus: 0
+      get bonus() {
+        return 0;
+      }
     };
     this.hand = [];
     this.equip = this.equip.bind(this);
